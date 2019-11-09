17fa613621f0ffaece16b8d0a3450f05b30a227e
nishkarsh <nishkarsh4@gmail.com>
2019-11-09 11:11:42 +0530

    Create HomeActivity with empty linear layout.

---
+++ app/src/main/java/com/intentfilter/people/views/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.intentfilter.people.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.intentfilter.people.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_home)
+    }
+}
