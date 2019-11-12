b97beb6ab536ff5e8c923cd65cf88d36c85baea0
nishkarsh <nishkarsh4@gmail.com>
2019-11-13 01:26:04 +0530

    Fix an issue with Parceler not recognising fields as non-private.

--- app/src/main/java/com/intentfilter/people/models/NamedAttribute.kt
+++ app/src/main/java/com/intentfilter/people/models/NamedAttribute.kt
@@ -2,10 +2,11 @@ package com.intentfilter.people.models
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.parceler.Parcel
+import org.parceler.Parcel.Serialization
 import org.parceler.ParcelConstructor
 import java.util.*
 
-@Parcel
+@Parcel(Serialization.BEAN)
 class NamedAttribute @ParcelConstructor constructor(
     @JsonProperty("id") var id: UUID,
     @JsonProperty("name") var name: String
